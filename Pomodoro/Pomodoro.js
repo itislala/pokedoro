fef7abae1ae74577428273c08366db50f8f2956c
Lais <lais2415@outlook.com>
2025-10-14 15:20:24 -0300

    [FIX] Contador pomodoro
    Ajuste do erro no decremento do cronometro do pomodoro e um ajuste de boas práticas do JS e HTML

--- Pomodoro/Pomodoro.js
+++ Pomodoro/Pomodoro.js
@@ -1,87 +1,87 @@
 // - - - Variáveis - - -
-const startStop = document.getElementById("startStop"); 
+const startStop = document.getElementById("startStop");
 const clock = document.getElementById("timer");
 
 const pomodoro = document.getElementById("pomodoro");
 const shortBreak = document.getElementById("shortBreak");
 let selectedTimer = "pomodoro";
+let timerParagraph = clock;
 
 let isRunning = false;
-let timeLeft = 1500;
 let interval;
+let secondsRemaining = null;
 
-function selectTimer(timer)
-{
-    selectedTimer = timer
-    changeSelectClasses(timer)
-    changeTimerValue(timer)
-}
-function changeSelectClasses(timer)
-{
-    if (timer == pomodoro)
-        {
-            pomodoro.classList.add("activeButton")
-            shortBreak.classList.remove("activeButton")
-        }
-    else if (timer == shortBreak)
-        {
-            pomodoro.classList.remove("activeButton")
-            shortBreak.classList.add("activeButton")
-        }
-}
-function changeTimerValue(timer)
-{
-    if(timer == pomodoro)
-    {
-        clock.textContent = secondsToMinutes(1500)
-    }
-    else if(timer == shortBreak)
-    {
-        clock.textContent = secondsToMinutes(300) 
+function selectTimer(timer) {
+    selectedTimer = timer;
+    changeSelectClasses(timer);
+    changeTimerValue(timer);
+    if (!isRunning) {
+        secondsRemaining = getTimerValue(selectedTimer);
     }
 }
+function changeSelectClasses(timer) {
+    if (timer == 'pomodoro') {
+        pomodoro.classList.add("activeButton");
+        shortBreak.classList.remove("activeButton");
+    }
+    else if (timer == 'shortBreak') {
+        pomodoro.classList.remove("activeButton");
+        shortBreak.classList.add("activeButton");
+    }
+}
+function getTimerValue(timer) {
+    return {
+        pomodoro: 25 * 60,
+        shortBreak: 5 * 60
+    }[timer];
+}
+function changeTimerValue(timer) {
+    timerParagraph.textContent = secondsToMinutes(getTimerValue(timer));
+}
+
 // - - - Contador - - -
 const stopTimer = () => clearInterval(interval);
-function secondsToMinutes(second)
-{
-    const minutes = Math.floor(second/60);
+
+function secondsToMinutes(second) {
+    const minutes = Math.floor(second / 60);
     const seconds = second % 60;
-    const padMinutes = minutes.toString().padStart(2,"0")
-    const padSeconds = seconds.toString().padStart(2,"0")
-    return `${padMinutes}:${padSeconds}`
+    const padMinutes = minutes.toString().padStart(2, "0");
+    const padSeconds = seconds.toString().padStart(2, "0");
+    return `${padMinutes}:${padSeconds}`;
 }
-const UpdateTimer = () =>
-{
-    /*const minutes = Math.floor(timeLeft/60);
-    const seconds = timeLeft % 60;
-    clock.innerHTML = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;*/
-}
-function StartStop (timer)
-{
-    let seconds
-    if (!isRunning)
-    {
+
+function StartStop() {
+    if (!isRunning) {
+        if (secondsRemaining === null) {
+            secondsRemaining = getTimerValue(selectedTimer);
+        }
+
         document.getElementById('startStop').textContent = 'Stop'
-        interval = setInterval(() => 
-        {    
-            timeLeft--;
-            clock.innerHTML = secondsToMinutes(seconds);
-            if (timeLeft == 0)
-            {
+        clock.textContent = secondsToMinutes(secondsRemaining)
+
+        interval = setInterval(() => {
+            secondsRemaining--;
+            clock.innerHTML = secondsToMinutes(secondsRemaining);
+            if (secondsRemaining == 0) {
                 clearInterval(interval);
                 alert("Time's up!");
-                clock.innerHTML = secondsToMinutes(seconds);
+                clock.innerHTML = secondsToMinutes(0);
+                isRunning = false;
+                startStop.textContent = "Start";
+                secondsRemaining = null
             }
-        }, 
+        },
             1000)
-        
     }
-    else
-    {
+    else {
         stopTimer();
-        document.getElementById('startStop').textContent = 'Start';
+        startStop.textContent = "Start";
     }
-    isRunning = !isRunning
+    isRunning = !isRunning;
 }
+pomodoro.addEventListener("click", () => selectTimer("pomodoro"));
+shortBreak.addEventListener("click", () => selectTimer("shortBreak"));
+startStop.addEventListener("click", StartStop);
 
-
+changeTimerValue(selectedTimer);
+secondsRemaining = getTimerValue(selectedTimer);
