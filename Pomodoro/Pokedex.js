722ecd6fb5da914d6fef0da804d4c630a3e5da16
Lais <lais2415@outlook.com>
2025-10-15 21:35:40 -0300

    [UPDT] Pokedex
    Atualização da pokedex e introdução da API

---
+++ Pomodoro/Pokedex.js
@@ -0,0 +1,6 @@
+const fetchPokemon = async (pokemon) =>{
+    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/pokemon${pokemon}`);
+    const data = await apiResponse.json();
+    return data;
+} 
+const renderPokemon
--- Pomodoro/ToDoList.js
+++ Pomodoro/ToDoList.js
@@ -1,63 +1,55 @@
 let tasks = []
-function addTask() 
-{   
+function addTask() {
     const inputTask = document.getElementById("inputTask")
     let task = inputTask.value.trim()
 
-    if (task == "")
-        {
-            alert("You need to type the task")
-        }
-    else
-        {
-            const message = document.getElementById("message")
-            let msgTaskAdded = "Task added successfully";
-            message.textContent = msgTaskAdded;
-            tasks.push(task)
-            renderTask()    
-        }
+    if (task == "") {
+        alert("You need to type the task")
+    }
+    else {
+        const message = document.getElementById("message")
+        let msgTaskAdded = "Task added successfully";
+        message.textContent = msgTaskAdded;
+        tasks.push(task)
+        renderTask()
+    }
     inputTask.value = ""
-        
+
 }
-function renderTask()
-{
+function renderTask() {
     const taskList = document.getElementById("taskList")
-    taskList.innerHTML =""
+    taskList.innerHTML = ""
     let i = 0
-    for(let i = 0; i<tasks.length; i++)
-        {
-            let newTask = document.createElement("li")
-            newTask.textContent = tasks[i]
+    for (let i = 0; i < tasks.length; i++) {
+        let newTask = document.createElement("li")
+        newTask.textContent = tasks[i]
 
-            //Botão de editar
-            let btEdit = document.createElement("button")
-            btEdit.className = "edit"
-            btEdit.textContent = "Edit"
-            btEdit.onclick = () => editTask(i)
+        //Botão de editar
+        let btEdit = document.createElement("button")
+        btEdit.className = "edit"
+        btEdit.textContent = "Edit"
+        btEdit.onclick = () => editTask(i)
 
-            //Botão de remover
-            let btRemove = document.createElement("button")
-            btRemove.className = "remove"
-            btRemove.textContent = "X"
-            btRemove.onclick = () => removeTask(i)
-            
-            newTask.appendChild(btRemove)
-            newTask.appendChild(btEdit)
-            taskList.appendChild(newTask)
-        }
+        //Botão de remover
+        let btRemove = document.createElement("button")
+        btRemove.className = "remove"
+        btRemove.textContent = "X"
+        btRemove.onclick = () => removeTask(i)
+
+        newTask.appendChild(btRemove)
+        newTask.appendChild(btEdit)
+        taskList.appendChild(newTask)
+    }
 }
-function removeTask(i)
-{
+function removeTask(i) {
     tasks.splice(i, 1)
     renderTask()
 }
-function editTask(i)
-{
+function editTask(i) {
     let edditedTask = prompt("Edit your task here")
-    if (edditedTask.trim !== "")
-        {
-            tasks[i] = edditedTask
-            renderTask()
-        }
-    
+    if (edditedTask.trim !== "") {
+        tasks[i] = edditedTask
+        renderTask()
+    }
+
 }
